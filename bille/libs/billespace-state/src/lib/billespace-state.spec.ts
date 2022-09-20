b4fc388070935837e28fbaabdbe3bf1c91bd20e1
Adrian Polubinski <adrian.polubinski@billennium.com>
2022-09-20 12:34:08 +0200

    Add state management library

---
+++ bille/libs/billespace-state/src/lib/billespace-state.spec.ts
@@ -0,0 +1,7 @@
+import { billespaceState } from './billespace-state';
+
+describe('billespaceState', () => {
+  it('should work', () => {
+    expect(billespaceState()).toEqual('billespace-state');
+  });
+});
---
+++ bille/libs/billespace-state/src/lib/billespace-state.ts
@@ -0,0 +1,3 @@
+export function billespaceState(): string {
+  return 'billespace-state';
+}
