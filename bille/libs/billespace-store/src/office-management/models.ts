6903fe8a142d76b4203600981048cacd4c42a289
Adrian Połubińskii <adrian.polubinski@billennium.com>
2022-11-24 13:09:22 +0100

    Add comments

--- bille/libs/billespace-store/src/office-management/models.ts
+++ bille/libs/billespace-store/src/office-management/models.ts
@@ -12,15 +12,19 @@ type EditData = CreationData & {
 // Current property is added to make state swap easier.
 export interface OfficeManagementReducerState {
   current:
-    | State<'IDLE'>
-    | State<'PREPARING'>
-    | State<'PREPARE_FAILED'>
-    | State<'CREATION', CreationData>
-    | State<'CREATING'>
-    | State<'CREATE_FAILED'>
-    | State<'CREATED'>
-    | State<'EDITION', EditData>
-    | State<'EDITING'>
-    | State<'EDIT_FAILED'>
-    | State<'EDITED'>;
+    | State<'IDLE'> // Nothing happens
+    | State<'PREPARING'> // Initial data starts loading
+    | State<'PREPARE_FAILED'> // Somethng went wrong with init data load
+    | State<'CREATION', CreationData> // Data loaded and user will populate form
+    | State<'CREATING'> // User finished and confirmed
+    | State<'CREATE_FAILED'> // Something wennt wrong when user trying to create office
+    | State<'CREATED'> // Office created - end of process
+    | State<'EDITION', EditData> // Data loaded for edit office, user will change pre-populated data
+    | State<'EDITING'> // User finished edition and confirmed
+    | State<'EDIT_FAILED'> // Something went wrong with edition
+    | State<'EDITED'>; // Office edited - process finished
 }
+
+export const INITIAL_STATE: OfficeManagementReducerState = {
+  current: { stage: 'IDLE' },
+};
