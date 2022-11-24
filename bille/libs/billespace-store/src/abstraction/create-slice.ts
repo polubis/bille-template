6283412853dc3a9de989a5da22f559139831b0b7
Adrian Połubińskii <adrian.polubinski@billennium.com>
2022-11-24 12:56:05 +0100

    Add abstractions

---
+++ bille/libs/billespace-store/src/abstraction/create-slice.ts
@@ -0,0 +1,23 @@
+import {
+  createSlice as toolkitCreateSlice,
+  SliceCaseReducers,
+  ValidateSliceCaseReducers,
+} from '@reduxjs/toolkit';
+
+export const createSlice = <
+  N extends string,
+  S,
+  R extends SliceCaseReducers<S>
+>(
+  name: N,
+  initialState: S,
+  reducers: ValidateSliceCaseReducers<S, R>
+) => {
+  const slice = toolkitCreateSlice({
+    name,
+    initialState,
+    reducers,
+  });
+
+  return [initialState, slice.reducer, slice.actions] as const;
+};
