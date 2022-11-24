4c59a401d8dcb208ac154f34bbaa5e76e5f7ff61
Adrian Połubińskii <adrian.polubinski@billennium.com>
2022-11-24 12:54:41 +0100

    Add form model

---
+++ bille/libs/billespace-store/src/office-management/config.ts
@@ -0,0 +1,39 @@
+import {
+  address,
+  inRange,
+  notEmpty,
+  ReduxToolkitForm,
+  required,
+  unique,
+} from '@bille/developer-kit';
+import { OfficePayload } from '@bille/billespace-services';
+
+export const [MIN_DESKS_COUNT, MAX_DESKS_COUNT] = [1, 100];
+export const [MIN_SPACES_COUNT, MAX_SPACES_COUNT] = [1, 100];
+
+export const { initial, set } = ReduxToolkitForm<OfficePayload>(
+  {
+    countryId: '',
+    cityId: '',
+    address: '',
+    postCode: '',
+    officeZones: [],
+    parkingZones: [],
+  },
+  {
+    countryId: [required()],
+    cityId: [required()],
+    address: [required(), address()],
+    postCode: [required()],
+    officeZones: [
+      notEmpty(),
+      inRange(MIN_DESKS_COUNT, MAX_DESKS_COUNT, 'desks'),
+      unique('name'),
+    ],
+    parkingZones: [
+      notEmpty(),
+      inRange(MIN_SPACES_COUNT, MAX_SPACES_COUNT, 'spaces'),
+      unique('name'),
+    ],
+  }
+);
